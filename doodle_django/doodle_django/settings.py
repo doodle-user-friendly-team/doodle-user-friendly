63b3b4752ccb323f42320a0f3bb86f87b707ede0
Beatrice Motta <mottabea@gmail.com>
2023-11-13 17:48:48 +0100

    [Feature]: Fix database settings

--- doodle_django/doodle_django/settings.py
+++ doodle_django/doodle_django/settings.py
@@ -74,26 +74,12 @@ WSGI_APPLICATION = "doodle_django.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-"""
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'database',
-        'USER': 'postgres',
-        'PASSWORD': 'your_db_password',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
