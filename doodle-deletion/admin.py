d18c036be7fe3ec50665faa7c3c29a3dffc7b855
tsion-19 <83315934+tsion-19@users.noreply.github.com>
2023-11-16 23:57:57 +0100

    Delete doodle-deletion directory

--- doodle-deletion/admin.py
+++
@@ -1,9 +0,0 @@
-
-# Register your models here.
-from django.contrib import admin
-from .models import Meeting
-
-@admin.register(Meeting)
-class MeetingAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'location', 'video_conferencing', 'duration', 'date_and_time', 'deadline')
-    search_fields = ('title', 'description')
