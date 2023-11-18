bad792085c735b8359a971e3e4daeefc606ed56f
uhth <vincenzorizzomy@gmail.com>
2023-11-18 14:48:42 +0100

    Fix deploy Add django tests

--- .docker/start_script.sh
+++ .docker/start_script.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:8000 & npm start --prefix doodle_react
+cd doodle_django
+npm run build --prefix doodle_react
+python3 manage.py collectstatic --noinput
+python3 manage.py runserver 0.0.0.0:8000
