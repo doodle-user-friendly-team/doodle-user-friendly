cda2aa83a8d27c5d2df3bc5384ff34d523b36232
tsion-19 <83315934+tsion-19@users.noreply.github.com>
2023-11-19 16:15:23 -0800

    tests.py

--- doodle_django/api/tests.py
+++ doodle_django/api/tests.py
@@ -4,19 +4,9 @@ from rest_framework.test import APITestCase
 from django.urls import reverse
 from django.utils.timezone import now, timedelta
 from collections import OrderedDict
-
-from django.test import TestCase
-from django.urls import reverse
 from .models import Meeting
-
-
-# myapp/tests.py
-
-from django.test import TestCase
-from django.urls import reverse
-from rest_framework import status
 from rest_framework.test import APIClient
-from .models import Meeting
+
 
 
 class MeetingTests(APITestCase):
