a40ad1b07d7b3b7bb9c8a52e5dba348ab8338f5a
Lorenzo Grillo <grillo383@gmail.com>
2023-11-20 11:35:33 +0100

    [Fix]: Test on get Timeslots

--- doodle_django/doodle/views.py
+++ doodle_django/doodle/views.py
@@ -10,7 +10,7 @@ from django.contrib.auth import authenticate, login
 from rest_framework.decorators import permission_classes, authentication_classes
 from rest_framework.permissions import AllowAny
 from rest_framework import status
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 class FormatError(Exception):
@@ -30,12 +30,12 @@ class TimeSlotView(APIView):
         day = request.data['day']
         month = request.data['month']
         year = request.data['year']
-        start_time = f"{int(year):04d}-{int(month) + 1:02d}-{int(day) - 1 :02d}T23:59:59Z"
+        start_time = f"{int(year):04d}-{int(month) + 1:02d}-{int(day) :02d}T23:59:59Z"
         end_time =  f"{int(year):04d}-{int(month) + 1:02d}-{(int(day)):02d}T00:00:00Z"
         
         try:
-            datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%SZ")
-            datetime.strptime(end_time, "%Y-%m-%dT%H:%M:%SZ")
+            start_time = datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%SZ") - timedelta(days=1)
+            end_time = datetime.strptime(end_time, "%Y-%m-%dT%H:%M:%SZ")
         except:
             raise FormatError
 
