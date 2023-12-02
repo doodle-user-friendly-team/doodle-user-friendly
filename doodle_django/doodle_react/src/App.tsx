5e66b9dbbf9cffaabc9f3fc882cb7c64016fe94d
Giacomo Galavotti <g.galavotti@protonmail.com>
2023-12-02 22:05:33 +0100

    test: revert last commits

--- doodle_django/doodle_react/src/App.tsx
+++ doodle_django/doodle_react/src/App.tsx
@@ -11,12 +11,13 @@ function App() {
         <Routes>
             {
                 Cookies.get('email') === undefined ?
-                    <Route path='/' element={<FormComponent />} />
+                    <>
+                        <Route path='/' element={<FormComponent />} />
+                    </>
                 :
                     <>
                         <Route path='/login' element={<FormComponent />}/>
                         <Route path='/' element={<PlanningPanelComponent />} />
-                        <Route path='/meeting' element={<Calendar currentDate={new Date(Date.now())} startDate={new Date("2023-11-30")} />} />
                     </>
             }
         </Routes>
