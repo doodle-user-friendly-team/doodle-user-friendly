df698475ec55486dc612ad7f63e9ced694dd4d24
AndreaYpmY <tocci.andrea15@gmail.com>
2023-12-04 13:28:36 +0100

    [FE] Add ViewTimeSlots component

---
+++ doodle_django/doodle_react/src/UI/MeetingRecap/Organizer/Component/RecapOrganizer.tsx
@@ -0,0 +1,36 @@
+
+import { ContainerTimeSlots } from "./ViewTimeSlots";
+import { MeetingRecap } from "./MeetingRecap";
+
+
+// PROP per ContainerTimeSlots
+interface TimeSlot{
+    id: number;
+    start_time: string;
+    end_time: string;
+    user: string; // nome della persona
+}
+interface Meeting{
+    start_meeting:string;
+    end_meeting: string
+    timeslots: TimeSlot[]
+}
+
+
+// PROP per MeetingRecap
+
+
+
+
+export function RecapOrganizer() {
+
+
+    /*
+    return (
+        <div>
+        <MeetingRecap  />
+        <ContainerTimeSlots />
+        </div>
+    );*/
+    return (<p></p>)
+}
