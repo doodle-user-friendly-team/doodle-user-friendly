61d36295bcd80ce9d8c5e85cee64f2c62da96102
Giacomo Galavotti <g.galavotti@protonmail.com>
2023-12-12 22:39:30 +0100

    fix(user-recap): add package-lock.json

--- doodle_django/doodle_react/src/App.tsx
+++ doodle_django/doodle_react/src/App.tsx
@@ -1,31 +1,37 @@
-import React from 'react';
-import { FormComponent } from './UI/Form/Component/FormIdentification';
-import logo from './logo.svg';
-import './App.css';
-import {PlanningPanelComponent} from "./UI/PlanningPanel/Component/PlanningPanel";
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Cookies from 'js-cookie';
-import Calendar from './UI/Calendar/Components/Calendar';
+import React from "react";
+import { FormComponent } from "./UI/Form/Component/FormIdentification";
+import logo from "./logo.svg";
+import "./App.css";
+import { PlanningPanelComponent } from "./UI/PlanningPanel/Component/PlanningPanel";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Cookies from "js-cookie";
+import Calendar from "./UI/Calendar/Components/Calendar";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 //{Cookies.get('email') === undefined && <FormComponent />}
 //{Cookies.get('email') !== undefined && <PlanningPanelComponent />}
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-            {
-                Cookies.get('email') === undefined ?
-                    <Route path='/' element={<FormComponent />} />
-                :
-                    <>
-                        <Route path='/login' element={<FormComponent />}/>
-                        <Route path='/' element={<PlanningPanelComponent />} />
-                        <Route path={'/schedulePool/:pool_link'} element={<Calendar />} />
-                    </>
-            }
-        </Routes>
-      </Router>
-    </div>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <div className="App">
+        <Router>
+          <Routes>
+            {Cookies.get("email") === undefined ? (
+              <Route path="/" element={<FormComponent />} />
+            ) : (
+              <>
+                <Route path="/login" element={<FormComponent />} />
+                <Route path="/" element={<PlanningPanelComponent />} />
+                <Route
+                  path={"/schedulePool/:pool_link"}
+                  element={<Calendar />}
+                />
+              </>
+            )}
+          </Routes>
+        </Router>
+      </div>
+    </LocalizationProvider>
   );
 }
 
