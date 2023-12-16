f3a52eedfdaa61ea0be59fb504416eb016ee9929
Jacopo Garofalo <jacopogarofalo02@gmail.com>
2023-12-16 18:11:34 +0100

    Revert "Merge branch 'group1_Sprint3' into group1_new_backend"
    This reverts commit 008685f3bdf48de0de7c7f1c656d3da74b35431d, reversing
    changes made to 6a8c7f719301567c495bd76d6062d163a9322079.

--- doodle_django/doodle/utils.py
+++ doodle_django/doodle/utils.py
@@ -1,11 +1,6 @@
 import string
 import random
-
-from django.template.loader import render_to_string
-from django.utils.html import strip_tags
-
 from .models import *
-from django.core.mail import send_mail, EmailMultiAlternatives
 
 
 def genera_stringa_casuale(lunghezza):
@@ -29,94 +24,3 @@ def genera_codice_invito():
         if not SchedulePool.objects.filter(pool_link=link).exists():
             break
     return link
-
-
-def send_meeting_creation_email(meeting):
-    link_organizer = "http://localhost:3000/recap-meeting/" + meeting.organizer_link
-
-    context = {
-        "user_name": meeting.user.name,
-        "name": meeting.name,
-        "meeting_link": link_organizer,
-    }
-
-    subject = "Doodle - Meeting created."
-    html_message = render_to_string("content/meeting_created.html", context=context)
-    plain_message = strip_tags(html_message)
-    message = EmailMultiAlternatives(
-        subject=subject,
-        body=plain_message,
-        from_email=None,
-        to=[meeting.user.email],
-    )
-
-    message.attach_alternative(html_message, "text/html")
-    message.send()
-
-
-def send_timeslot_creation_email(schedule_pool, timeslot):
-
-    users_emails = []
-
-    date = timeslot.start_time.strftime("%d/%m/%Y")
-    time = timeslot.start_time.strftime("%H:%M") + " - " + timeslot.end_time.strftime("%H:%M")
-
-    for ts in schedule_pool.timeslot_set:
-        if ts.user.email not in users_emails and ts.user.email != timeslot.user.email:
-            users_emails.append(ts.user.email + " " + ts.user.name)
-        for p in ts.vote_set:
-            if p.user.email not in users_emails and p.user.email != timeslot.user.email:
-                users_emails.append(p.user.email + " " + p.user.name)
-
-    link_participant = "http://localhost:3000/vote/" + schedule_pool.pool_link
-
-    for u in users_emails:
-
-        splitted_data = u.split(" ")
-
-        context = {
-            "user_name": splitted_data[1],
-            "name": schedule_pool.meeting.name,
-            "date": date,
-            "time": time,
-            "vote_link": link_participant,
-        }
-
-        subject = "Doodle - TimeSlot created."
-        html_message = render_to_string("content/timeslot_created.html", context=context)
-        plain_message = strip_tags(html_message)
-        message = EmailMultiAlternatives(
-            subject=subject,
-            body=plain_message,
-            from_email=None,
-            to=[splitted_data[0]],
-        )
-
-        message.attach_alternative(html_message, "text/html")
-        message.send()
-
-
-def send_preference_creation_email(vote):
-    link_participant = "http://localhost:3000/vote/" + vote.time_slot.schedule_pool.pool_link
-
-    context = {
-        "user_name": vote.timeslot.user.name,
-        "meeting_name": vote.timeslot.schedule_pool.meeting.name,
-        "preference": vote.preference,
-        "vote_link": link_participant,
-    }
-
-    subject = "Doodle - Preference created."
-    html_message = render_to_string("content/preference_created.html", context=context)
-    plain_message = strip_tags(html_message)
-    message = EmailMultiAlternatives(
-        subject=subject,
-        body=plain_message,
-        from_email=None,
-        to=[vote.timeslot.user.email],
-    )
-
-    message.attach_alternative(html_message, "text/html")
-    message.send()
-
-
--- doodle_django/doodle_django/urls.py
+++ doodle_django/doodle_django/urls.py
@@ -34,26 +34,4 @@ urlpatterns = [
     path("api/v1/authenticatedMeetings/", AuthMeetingView.as_view()),
     path('api/v1/auth/registration/', include('dj_rest_auth.registration.urls')),
     path('api/v1/authenticate/', djangoUsers.as_view(), name='user_authentication'),
-
-
-    path('api/v1/top_timeslots/<str:link>', TopTimeSlotsView.as_view(), name='top_timeslots'),
-
-    path('api/v1/timeslots/id/<int:time_slot_id>', get_timeslot, name='timeslot_vote_list'),
-    path('api/v1/schedule_pool/<str:link>', SchedulePoolView.as_view(), name='schedule_pool'),
-    path('api/v1/schedule_pool/timeslots/<int:schedule_pool_id>', get_timeslot_from_schedule_pool, name='timeslot_from_sschedule_pool'),
-
-    path('api/v1/update_preference/', ModifyMyPreferenceView.as_view(),name='update_preference'),
-    path('api/v1/votes/timeslot/<str:time_slot_id>', get_preferences, name='timeslot_vote_list'),
-
-    path('api/v1/users/<int:user_id>', UserByIdView.as_view(), name='get_user_by_id'),
-    path('api/v1/users/schedulepool/<int:user_id>', get_schedule_pool_from_user_id, name='get_schedule_pool_from_user_id'),
-
-    path('accounts/', include('allauth.urls')),
-    path('logout', LogoutView.as_view()),
-
-    # add react path
-    # but I think it's better to do it when we have finished the website
-    # since we have to build the react app first
-
-    path('send_link_by_email/<int:meeting_id>/', send_link_by_email, name='send_link_by_email'),
 ]
